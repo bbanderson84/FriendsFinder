f552220c410105b8b635e5a92af6c3e66291e1c4
bbanderson84 <bbanderson84@gmail.com>
2019-03-20 11:04:19 -0600

    added files

---
+++ app/data/friends.js
@@ -0,0 +1,166 @@
+// Array that will store friends data
+
+var friendsArr = [
+    {
+		name: 'Jerry',
+		photo: 'http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-15.jpg',
+		scores: [
+			'1',
+			'3',
+			'2',
+			'1',
+			'5',
+			'5',
+			'2',
+			'1',
+			'1',
+			'5'
+		]
+    },
+    {
+		name: 'Harry',
+		photo: 'http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-15.jpg',
+		scores: [
+			'1',
+			'3',
+			'5',
+			'1',
+			'2',
+			'5',
+			'3',
+			'1',
+			'2',
+			'1'
+		]
+    },
+    {
+		name: 'Clarise',
+		photo: 'http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-15.jpg',
+		scores: [
+			'3',
+			'2',
+			'2',
+			'4',
+			'3',
+			'3',
+			'2',
+			'2',
+			'1',
+			'5'
+		]
+    },
+    {
+		name: 'Sally',
+		photo: 'http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-15.jpg',
+		scores: [
+			'2',
+			'2',
+			'2',
+			'2',
+			'3',
+			'4',
+			'2',
+			'1',
+			'1',
+			'5'
+		]
+    },
+    {
+		name: 'Janice',
+		photo: 'http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-15.jpg',
+		scores: [
+			'5',
+			'4',
+			'3',
+			'2',
+			'4',
+			'5',
+			'2',
+			'1',
+			'2',
+			'3'
+		]
+    },
+    {
+		name: 'Craig',
+		photo: 'http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-15.jpg',
+		scores: [
+			'5',
+			'5',
+			'2',
+			'4',
+			'4',
+			'4',
+			'4',
+			'4',
+			'4',
+			'2'
+		]
+    },
+    {
+		name: 'Lexi',
+		photo: 'http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-15.jpg',
+		scores: [
+			'2',
+			'3',
+			'3',
+			'4',
+			'4',
+			'3',
+			'1',
+			'2',
+			'1',
+			'4'
+		]
+    },
+    {
+		name: 'Ron',
+		photo: 'http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-15.jpg',
+		scores: [
+			'2',
+			'5',
+			'4',
+			'2',
+			'4',
+			'1',
+			'4',
+			'1',
+			'5',
+			'1'
+		]
+    },
+    {
+		name: 'Kristin',
+		photo: 'http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-15.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'4',
+			'3',
+			'4',
+			'5',
+			'4',
+			'3',
+			'5'
+		]
+    },
+    {
+		name: 'Kevo',
+		photo: 'http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-15.jpg',
+		scores: [
+			'1',
+			'2',
+			'2',
+			'3',
+			'2',
+			'1',
+			'2',
+			'1',
+			'1',
+			'3'
+		]
+	},
+];
+
+module.exports = friendsArr;
