2fe588130a2210e8455967fe22883941b4562880
bbanderson84 <bbanderson84@gmail.com>
2019-03-24 23:06:03 -0600

    added updates

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,22 +1,54 @@
 //LOAD DATA
 //Linking routes to data sources
 
-var friendsArr = require("..data/friends.js");
+var friendsArr = require("../data/friends.js");
 
 module.exports = function(app){
     //API GET requests - below code handles when users visit a page
 
-    app.get("/api/friends.js", function(req, res) {
+    app.get("/api/friends", function(req, res) {
+
         res.json(friendsArr);
 
     });
 
-    app.post("/api/friends.js", function (req, res) {
+    app.post("/api/friends", function (req, res) {
 
         var userInput = req.body;
 
-        var userResponses = userInput.scores;
-        
+        var userAnswers = userInput.scores;
 
-    }
-}
+        var matchName = "";
+
+        var matchImage = '';
+
+        var setDifference = 2000;
+
+        for (var i = 0; i < friendsArr.length; i++) {
+
+            var difference = 0;
+
+            for (var a = 0; a < userAnswers.length; a++) {
+                difference += Math.abs(friendsArr[i].scores[a] - userAnswers[a]);
+
+            }
+
+            if (difference < setDifference ) {
+                
+                setDifference = difference;
+
+                matchName = friendsArr[i].name;
+
+                matchImage = friendsArr[i].photo;
+            }
+
+        }
+
+        friendsArr.push(userInput);
+
+        res.json({status: 'OK', matchName: matchName, matchImage: matchImage});
+
+
+
+    });
+};
--- server.js
+++ server.js
@@ -6,14 +6,16 @@ var path = require('path');
 var app = express();
 var PORT = 8080;
 
+app.use(express.static(path.join(__dirname, './app/public')));
+
 // Sets up express app to handle data parsing
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 //ROUTER - points to  server to a series of "route" files.
 //Gives server a map of how to respond when users visit or request HTML
-require("./routing/apiRoutes")(app);
-require("./routing/htmlRoutes")(app);
+require(path.join(__dirname, "./app/routing/apiRoutes.js"))(app);
+require(path.join(__dirname,"./app/routing/htmlRoutes.js"))(app);
 
 //LISTENER - below code "starts" the server
 app.listen(PORT, function() {
